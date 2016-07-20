59e11e381152334171fe7dd2526fee4cbdd23431
simmy 刘 <simmy@simmydemacbook-air.local>
2016-07-21 00:19:40 +0800

    hgjhgh

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically fr.
+        // Do any additional setup after loading the view, typically fr.jhjkhjhjhjh
     }
 
     override func didReceiveMemoryWarning() {
